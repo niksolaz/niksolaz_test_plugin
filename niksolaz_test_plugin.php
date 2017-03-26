5f01cf6a431cb320ab7e2c50a1945727e5ced491
niksolaz <solazzo.nicola@gmail.com>
2017-03-26 20:08:22 +0200

    impostazione base plugin

--- niksolaz_test_plugin.php
+++ niksolaz_test_plugin.php
@@ -4,5 +4,12 @@ Plugin Name: niksolaz_test_plugin
 Version: 1.0
 Description: Descrizione del plugin
 Author: Nicola Solazzo
-Author URI: http://https://github.com/niksolaz/niksolaz_test_plugin
+Author URI: http://www.nicolasolazzo.com
 */
+
+define( 'MY_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define( 'MY_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+require_once(MY_PLUGIN_PATH . '/niksola_test_plugin.php');
+
+wp_register_script( ‘my’, MY_PLUGIN_URL . ‘js/my.js’, array( ‘jquery’ ), ‘1.0’, true );
