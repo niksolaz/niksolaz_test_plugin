7a7d50bd5df5fc29162d9eb6b24eb9ef9bb6b455
niksolaz <solazzo.nicola@gmail.com>
2017-03-26 15:54:37 +0200

    begin plugin

---
+++ niksolaz_test_plugin.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: niksolaz_test_plugin
+Version: 1.0
+Description: Descrizione del plugin
+Author: Nicola Solazzo
+Author URI: http://https://github.com/niksolaz/niksolaz_test_plugin
+*/
